af1aad949eb27b8fdd8756a2c5d5a9cc9438f8ee
TranThiTho2001 <tranthitho09052001@gmail.com>
2023-05-15 21:04:30 +0700

    cai dat server

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,42 +1,7 @@
-// module.exports = {
-//   devServer: {
-//     port: 8081,
-//     host: '0.0.0.0'
-//   },
-// };
-// // // // };
-// const path = require("path");
 
 const webpack = require('webpack')
 const { VueLoaderPlugin } = require('vue-loader')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-// module.exports = {
-//   // mode: 'development',
-//   devServer: {
-//     port: 8081,
-//     host: '0.0.0.0'
-//   },
-
-// //   module: {
-// //     rules: [
-// //       {
-// //         test: /\.tsx?$/,
-// //         use: 'ts-loader',
-// //         exclude: /node_modules/,
-// //       },
-// //     ],
-// //   },
-// //   resolve: {
-// //     alias: {
-// //       vue: "vue/dist/vue.esm-bundler.js"
-// //     },
-// //     extensions: ['.tsx', '.ts', '.js'],
-// //   },
-// //   output: {
-// //     filename: 'bundle.js',
-// //     path: path.resolve(__dirname, 'dist'),
-// //   }
-// // }
 module.exports = {
     devServer: {
           port: 8081,
